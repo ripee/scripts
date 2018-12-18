9b9eb8cdbca4f8cf59f2f8c715c25194f4c98188
ripee <ripee.dev@gmail.com>
2018-12-18 01:26:01 +0100

    Relegate autoremove to a system cleaner
    and dist-upgrade and update to the linux distro

--- build_environment.sh
+++ build_environment.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-apt-get -y dist-upgrade
-apt-get -y update
 apt-get -y remove openjdk-* icedtea-* icedtea6-*
-apt-get -y autoremove
 apt-get -y install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop openjdk-8-jdk pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 mkdir ~/bin
 curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
@@ -11,7 +8,7 @@ chmod a+x ~/bin/repo
 ccache -M 100G
 cd ~/.jack-server
 sed -i 's/jack.server.max-service=4/jack.server.max-service=1/' config.properties
-cd ~
+cd
 echo 'export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx12G"' >> ~/.bashrc
 echo 'export LANG=C' >> ~/.bashrc
 echo 'export PATH=~/bin:$PATH' >> ~/.bashrc
