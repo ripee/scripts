3881cbafe818a441ecf26e9f5c431bb062839bc1
ripee <ripee.dev@gmail.com>
2019-01-06 01:38:32 +0100

    twrp directory error

--- twrp.sh
+++ twrp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/android/twrp
-cd ~/twrp
+cd ~/android/twrp
 repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-7.1
 repo sync -c --force-sync --no-clone-bundle --no-tags
 mkdir -p .repo/local_manifests
