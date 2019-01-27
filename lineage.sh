e2ffd9c872464a7e23557e25efe5c44adfba5c29
ripee <ripee.dev@gmail.com>
2019-01-27 22:48:08 +0100

    Sync down both 16.0 and 15.1

--- lineage.sh
+++ lineage.sh
@@ -2,6 +2,8 @@
 
 mkdir -p ~/android/lineage
 cd ~/android/lineage
+repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-16.0
+repo sync -c --force-sync --no-clone-bundle --no-tags
 repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-15.1
 repo sync -c --force-sync --no-clone-bundle --no-tags
 mkdir -p .repo/local_manifests
