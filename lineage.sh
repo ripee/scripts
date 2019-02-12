f10b5dd929547814ebb1321bc4a7dc9ab4bfb297
Flashaholicturbo <flashaholic@turbolearning.co.uk>
2019-02-12 20:34:28 +0100

    LOS "fix"

--- lineage.sh
+++ lineage.sh
@@ -2,6 +2,8 @@
 
 mkdir -p ~/android/lineage
 cd ~/android/lineage
+## LOS16 ## dont work with lineage 16.0 and cherrypicks (descripted by ivan /local_manifests) / just use repo sync/repo init
+## it compiles just fine and boots up but if you wanna setup wifi and google it constantly reboots
 repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-16.0
 repo sync -c --force-sync --no-clone-bundle --no-tags
 repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-15.1
