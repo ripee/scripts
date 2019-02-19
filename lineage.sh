42d394eb501f271ad2adb9e9c3a21d662117d4c6
ripee <ripee.dev@gmail.com>
2019-02-19 20:58:22 +0100

    Deprecate lineage-15.1
    So long, Oreo, and we thank you.

--- lineage.sh
+++ lineage.sh
@@ -4,7 +4,5 @@ mkdir -p ~/android/lineage
 cd ~/android/lineage
 repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-16.0
 repo sync -c --force-sync --no-clone-bundle --no-tags
-repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-15.1
-repo sync -c --force-sync --no-clone-bundle --no-tags
 mkdir -p .repo/local_manifests
 cd ~
