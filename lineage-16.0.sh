fe8bf9f6342666a8e257ad175e5caae56b7045ad
ripee <ripee.dev@gmail.com>
2019-04-24 01:07:56 +0200

    Specify rom branches

--- lineage-16.0.sh
+++ lineage-16.0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p ~/android/lineage
-cd ~/android/lineage
+mkdir -p ~/android/lineage-16.0
+cd ~/android/lineage-16.0
 repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-16.0
 repo sync -c --force-sync --no-clone-bundle --no-tags
 mkdir -p .repo/local_manifests
--- twrp-7.1.sh
+++ twrp-7.1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p ~/android/twrp
-cd ~/android/twrp
+mkdir -p ~/android/twrp-7.1
+cd ~/android/twrp-7.1
 repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-7.1
 repo sync -c --force-sync --no-clone-bundle --no-tags
 mkdir -p .repo/local_manifests
