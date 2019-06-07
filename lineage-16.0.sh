d23c325bde380c5c30fd280ad8a390a3e84fa77f
ripee <ripee.dev@gmail.com>
2019-06-07 23:10:23 +0200

    Simplify change directory to /home/ripee

--- lineage-16.0.sh
+++ lineage-16.0.sh
@@ -5,4 +5,4 @@ cd ~/android/lineage-16.0
 repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-16.0
 repo sync -c --force-sync --no-clone-bundle --no-tags
 mkdir -p .repo/local_manifests
-cd ~
+cd
--- twrp-7.1.sh
+++ twrp-7.1.sh
@@ -5,4 +5,4 @@ cd ~/android/twrp-7.1
 repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-7.1
 repo sync -c --force-sync --no-clone-bundle --no-tags
 mkdir -p .repo/local_manifests
-cd ~
+cd
