f844f21bd034f63b934d4b6eaf1015ebf89a19c4
ripee <ripee.dev@gmail.com>
2019-07-17 21:33:26 +0200

    Update gdrive download URL

--- gdrive.sh
+++ gdrive.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
+wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE
 mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
 sudo chmod a+x gdrive
 sudo install gdrive /usr/local/bin/gdrive
+rm gdrive
 gdrive list
