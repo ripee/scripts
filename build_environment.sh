f4f381117ebc2ff99e3ba6f226e9461c76b49877
KeepSOBP <50764666+KeepSOBP@users.noreply.github.com>
2019-10-24 20:15:41 +0900

    added upgrade and improve cpu performance.
    you need reboot.

--- build_environment.sh
+++ build_environment.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+apt-get update
+apt-get -y dist-upgrade
 apt-get -y remove openjdk-* icedtea-* icedtea6-*
-apt-get -y install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop openjdk-8-jdk pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+apt-get -y install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop openjdk-8-jdk pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev linux-tools-5.0.0-32-generic linux-cloud-tools-5.0.0-31-generic linux-tools-generic linux-cloud-tools-generic
+apt-get -y autoremove
 mkdir ~/bin
 curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
@@ -11,4 +14,5 @@ echo 'export LANG=C' >> ~/.bashrc
 echo 'export PATH=~/bin:$PATH' >> ~/.bashrc
 echo 'export USE_CCACHE=1' >> ~/.bashrc
 . ~/.bashrc
+cpupower frequency-set -g performance
 mkdir ~/android
