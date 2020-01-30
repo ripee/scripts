091b22335190e84bb27e1fd7f36393da6fe66d2a
ripee <ripee.dev@gmail.com>
2020-01-30 23:03:36 +0100

    Rename to 17.1

--- lineage-16.0.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/android/lineage-16.0
-cd ~/android/lineage-16.0
-repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-16.0
-repo sync -c --force-sync --no-clone-bundle --no-tags
-mkdir -p .repo/local_manifests
-cd
