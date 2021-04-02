af5af6f4de7f93981cdddea9459c1ce26fec9387
ripee <ripee.dev@gmail.com>
2021-04-02 11:37:35 +0200

    Bump 17.1 to 18.1

--- lineage-17.1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/android/lineage-17.1
-cd ~/android/lineage-17.1
-repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-17.1
-repo sync -c --force-sync --no-clone-bundle --no-tags
-mkdir -p .repo/local_manifests
-cd
