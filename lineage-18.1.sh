5b234f988144ba5fdd501edc10d2e6c9c51b450e
ripee <ripee.dev@gmail.com>
2022-08-14 19:15:28 +0200

    Switch to https

--- lineage-18.1.sh
+++ lineage-18.1.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/android/lineage-18.1
 cd ~/android/lineage-18.1
-repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1
+repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-18.1
 repo sync -c --force-sync --no-clone-bundle --no-tags
 mkdir -p .repo/local_manifests
 cd
--- twrp-7.1.sh
+++ twrp-7.1.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/android/twrp-7.1
 cd ~/android/twrp-7.1
-repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-7.1
+repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-7.1
 repo sync -c --force-sync --no-clone-bundle --no-tags
 mkdir -p .repo/local_manifests
 cd
