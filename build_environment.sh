e20546afc9adfc51971a045ed89d48f283baa4f9
ripee <ripee.dev@gmail.com>
2023-10-22 12:58:41 +0200

    Change repo source URL

--- build_environment.sh
+++ build_environment.sh
@@ -7,7 +7,7 @@ sudo apt -y remove openjdk-* icedtea-* icedtea6-*
 sudo apt -y install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop openjdk-8-jdk pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 sudo apt -y autoremove
 mkdir ~/bin
-curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 ccache -M 100G
 echo 'export ANDROID_COMPILE_WITH_JACK=false' >> ~/.bashrc
